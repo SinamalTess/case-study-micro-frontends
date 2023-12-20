6cdef5ea01a25885a709923ed2ee90c1992f9300
Tess Sinamal <tess.sinamal@gmail.com>
2023-12-20 17:21:53 +0100

    doc: introduction notes

---
+++ packages/react/index.jsx
@@ -0,0 +1,4 @@
+import react from 'react'
+
+export const ReactApp = () =>
+  <h1>Hello World</h1>
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mode: 'development',
+    entry: "./packages/container/index.js",
+    devServer: {
+        port: 8081
+    }
+}
